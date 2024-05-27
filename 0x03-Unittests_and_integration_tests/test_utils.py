d230ea29b221772aafdb6eadba4ff5ef48366806
wahujos <jwahu30@gmail.com>
2024-05-27 21:01:38 +0300

    question 1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -25,8 +25,8 @@ class TestAccessNestedMap(unittest.TestCase):
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([
-        ({}, ("a,"), "a"),
-        ({"a": 1}, ("a", "b"), "b")
+        ({}, ("a,"), "'a'"),
+        ({"a": 1}, ("a", "b"), "'b'")
     ])
     def test_access_nested_map_exception(self, nested_map, path, missing_key):
         """
