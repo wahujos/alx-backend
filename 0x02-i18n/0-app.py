c66a06d648d65391bf5f27c26b3177c2f6f39424
wahujos <jwahu30@gmail.com>
2024-05-31 18:41:12 +0300

    0-app.py

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def index():
+    """index page"""
+    return render_template("0-index.html")
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
