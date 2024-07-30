fe4332071340d4750a2bc5bf7e685ddda7e73e39
wahujos <jwahu30@gmail.com>
2024-07-31 02:35:12 +0300

    6-job_creator.js

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,23 @@
+const kue = require('kue');
+const queue = kue.createQueue();
+
+
+const jobData = {
+    phoneNumber: '123456789',
+    message: 'Successful',
+}
+
+const job = queue.create('push_notification_code', jobData)
+    .save((err) => {
+        if(!err) {
+            console.log(`Notification job created: ${job.id}`);
+        }
+    });
+
+job.on('complete', () => {
+    console.log(`Notification job completed`);
+});
+
+job.on('failed', () => {
+    console.log(`Notification job failed`);
+});
